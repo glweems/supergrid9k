052db299918f3105c2b529f2d99e0072a3169d2e
Garrett Weems <gwgraphicdesign@gmail.com>
2020-08-03 14:06:04 -0500

    :bug: add keys to icon paths array

--- src/lib/Icons.tsx
+++ src/lib/Icons.tsx
@@ -22,10 +22,12 @@ export const PlusIcon: React.FC<IconProps> = (props) =>
     ...props,
     children: [
       <path
+        key="PlusIconPath-1"
         fillRule="evenodd"
         d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"
       />,
       <path
+        key="PlusIconPath-2"
         fillRule="evenodd"
         d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"
       />,
@@ -37,10 +39,12 @@ export const CloseIcon: React.FC<IconProps> = (props) =>
     ...props,
     children: [
       <path
+        key="CloseIconPath-1"
         fillRule="evenodd"
         d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"
       />,
       <path
+        key="CloseIconPath-2"
         fillRule="evenodd"
         d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"
       />,
