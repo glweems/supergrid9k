58afe28fb2c40f2980c7e29a1ac8fb478009d25a
Garrett Weems <gwgraphicdesign@gmail.com>
2020-09-16 13:17:45 -0500

    GridEditorCard url

--- components/GridEditorCard.tsx
+++ components/GridEditorCard.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Card, Heading, Image, Link } from 'rebass/styled-components';
-import { hostingURL } from '../lib/appConfig';
 import NextLink from 'next/link';
 interface GridEditorCardProps {
   id: string;
 }
 
 const GridEditorCard = ({ id }: GridEditorCardProps) => {
-  const url = `${hostingURL}/api/tbn/${id}`;
+  const url = `/api/tbn/${id}`;
   return (
     <Card>
       <Image src={url} />
