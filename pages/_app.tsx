3d3ea59f9907ac674f315a4cb1f888920581193a
Garrett Weems <gwgraphicdesign@gmail.com>
2020-09-28 10:59:59 -0500

    update favicon.ico

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,14 @@ import { RecoilRoot } from 'recoil';
 import ContextProvider from '@/components/ContextProvider';
 import whyDidYouRender from '@welldone-software/why-did-you-render';
 
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
-  whyDidYouRender(React);
-}
-
 import {
   setCssArrayVariables,
   setCssObjectVariables,
 } from '@/lib/setCssVariables';
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+  whyDidYouRender(React);
+}
 
 class MyApp extends App<AppProps<{ dehydratedState: any }>> {
   static async getInitialProps({ Component, ctx }: AppContext) {
@@ -38,6 +38,24 @@ class MyApp extends App<AppProps<{ dehydratedState: any }>> {
       <React.Fragment>
         <Head>
           <title>Super Grid 9K</title>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <RecoilRoot>
           <ContextProvider>
