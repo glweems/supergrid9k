00224e443d20f058e0a002681a710f7b0760f914
Garrett Weems <gwgraphicdesign@gmail.com>
2020-10-21 05:08:26 -0500

    delete old code

--- Grid/old/index.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'recoil';
-import { GridState } from 'css-grid-template-parser';
-
-export const gridState = atom<GridState | null>({ key: '', default: null });
-export type GridControlsKey = 'rows' | 'columns';
